fc1c3077acf802c7c8d590bfcdfebebab3fc5b07
bala-amypo <bala@amypo.in>
2025-12-19 09:30:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/FinancialProfileService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.FinancialProfile;
+
+public interface FinancialProfileService {
+    FinancialProfile createOrUpdate(FinancialProfile profile);
+    FinancialProfile getByUserId(Long userId);
+}

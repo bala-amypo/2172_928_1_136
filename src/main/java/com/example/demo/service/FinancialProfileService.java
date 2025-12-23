54c9199d890bfa93610e0d3d81fe6c73352a3013
bala-amypo <bala@amypo.in>
2025-12-23 10:03:30 +0000

    User commit

--- src/main/java/com/example/demo/service/FinancialProfileService.java
+++ src/main/java/com/example/demo/service/FinancialProfileService.java
@@ -1,26 +1,10 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
 import com.example.demo.entity.FinancialProfile;
-import com.example.demo.repository.FinancialProfileRepository;
-import com.example.demo.service.FinancialProfileService;
-import org.springframework.stereotype.Service;
 
-@Service
-public class FinancialProfileServiceImpl implements FinancialProfileService {
+public interface FinancialProfileService {
 
-    private final FinancialProfileRepository profileRepository;
+    FinancialProfile create(FinancialProfile profile);
 
-    public FinancialProfileServiceImpl(FinancialProfileRepository profileRepository) {
-        this.profileRepository = profileRepository;
-    }
-
-    @Override
-    public FinancialProfile create(FinancialProfile profile) {
-        return profileRepository.save(profile);
-    }
-
-    @Override
-    public FinancialProfile getById(Long id) {
-        return profileRepository.findById(id).orElse(null);
-    }
+    FinancialProfile getById(Long id);
 }
